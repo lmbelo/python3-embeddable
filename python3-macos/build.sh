70af3b069eb86110d36f53e4587fb6cb30b0ce7b
lmbelo <lucas.belo@live.com>
2022-04-26 13:27:14 -0700

    Make and install python from source - it generates the embeddable

--- python3-macos/build.sh
+++ python3-macos/build.sh
@@ -6,6 +6,9 @@ set -x
 # Install requirements
 brew install xz
 
+#Initialize variables
+THIS_DIR="$PWD"
+
 # Create the Python source dir
 mkdir -p src
 pushd src
@@ -14,6 +17,10 @@ pushd src
 curl -vLO https://www.python.org/ftp/python/$PYVER/Python-$PYVER.tar.xz
 tar --no-same-owner -xf Python-$PYVER.tar.xz
 
+# Configure and make Python from source
+./configure --prefix=/usr "$@" --enable-shared
+make
+make install DESTDIR="$THIS_DIR/build"
+
 popd src
 
-ls -R
