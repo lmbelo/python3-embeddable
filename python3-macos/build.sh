78ad1f0ef58d6aa40e70a00bbda5ee7aa5566fff
lmbelo <lucas.belo@live.com>
2022-04-26 13:07:24 -0700

    Downloading and extracting Python source

---
+++ python3-macos/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# Create the Python source dir
+mkdir -p src
+pushd src
+
+# Download Python
+curl -vLO https://www.python.org/ftp/python/$PYVER/Python-$PYVER.tar.xz
+tar --no-same-owner -xf Python-$PYVER.tar.xz
+
+popd src
+
+ls -R
