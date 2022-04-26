6a5bc32ea6088ee5da491c3b450ef435b15c2da9
lmbelo <lucas.belo@live.com>
2022-04-26 13:30:23 -0700

    Moving into the Python source dir before calling the configure and make routines

--- python3-macos/build.sh
+++ python3-macos/build.sh
@@ -8,6 +8,7 @@ brew install xz
 
 #Initialize variables
 THIS_DIR="$PWD"
+PY_SRC_DIR=src/Python-$PYVER
 
 # Create the Python source dir
 mkdir -p src
@@ -17,10 +18,13 @@ pushd src
 curl -vLO https://www.python.org/ftp/python/$PYVER/Python-$PYVER.tar.xz
 tar --no-same-owner -xf Python-$PYVER.tar.xz
 
+popd src
+
+pushd $PY_SRC_DIR
+
 # Configure and make Python from source
 ./configure --prefix=/usr "$@" --enable-shared
 make
 make install DESTDIR="$THIS_DIR/build"
 
-popd src
-
+popd
