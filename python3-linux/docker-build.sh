90302fcc19692745e4e8298e20ca048a425b91a8
Lucas Moura Belo <lucas.belo@live.com>
2022-10-14 11:36:40 -0300

    Installing C compiler tools

--- python3-linux/docker-build.sh
+++ python3-linux/docker-build.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 apt-get update -y
-apt-get install -y autoconf autoconf-archive automake cmake gawk gettext git gcc make patch pkg-config
+apt-get install -y autoconf autoconf-archive gawk gettext git patch pkg-config build-essential
 
 cd /python3-linux
 
