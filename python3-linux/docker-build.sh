305c52769169f30a1a4fbebaa69247c01e119c6f
Lucas Moura Belo <lucas.belo@live.com>
2022-10-14 12:39:44 -0300

    Removing pre-installed openssl

--- python3-linux/docker-build.sh
+++ python3-linux/docker-build.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 apt-get update -y
-apt-get remove openssl
+apt-get remove openssl -y
 apt-get install -y autoconf autoconf-archive gawk gettext git patch pkg-config build-essential
 
 cd /python3-linux
