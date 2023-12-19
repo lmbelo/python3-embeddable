f8cc7dbf4e47b2aa350bf26158e9cc8e247cfb8a
Lucas Moura Belo <lucas.belo@live.com>
2023-12-19 15:15:31 -0300

    Update docker-build.sh

--- python3-android/docker-build.sh
+++ python3-android/docker-build.sh
@@ -11,7 +11,7 @@ export ANDROID_NDK=/android-ndk
 if [ ! -d "$ANDROID_NDK" ] ; then
     # In general we don't want download NDK for every build, but it is simpler to do it here
     # for CI builds
-    NDK_VER=r21d
+    NDK_VER=r26b
     apt-get install -y wget unzip #bsdtar    
     wget --no-verbose https://dl.google.com/android/repository/android-ndk-$NDK_VER-linux-x86_64.zip
     # bsdtar xf android-ndk-${NDK_VER}-linux-x86_64.zip
